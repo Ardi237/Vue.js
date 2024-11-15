9a770ecbb8b71a816cccd0ebf6364549c5eaa843
Ardi Okdianto <118036427+Ardi237@users.noreply.github.com>
2024-11-15 21:05:58 +0700

    Update 3. Perulangan di Vue.js

--- 3. Perulangan di Vue.js
+++ 3. Perulangan di Vue.js
@@ -1,1 +1,44 @@
+A. Perulangan d Vue.js :
+1. v-for adalah direktif dalam Vue.js yang digunakan untuk membuat perulangan di dalam template.
+--------------------------------------------------------------------------------------------------------------------
+2. kita dapat menampilkan daftar data atau elemen berdasarkan array atau objek yang ada dalam data komponen. 
+--------------------------------------------------------------------------------------------------------------------
+3. Fitur ini sangat berguna untuk menangani daftar item, tabel dinamis, atau elemen berulang lainnya.
+--------------------------------------------------------------------------------------------------------------------
+
+
+
+B. Sintaks Dasar v-for :
+--------
+<div v-for="item in items" :key="item.id">
+  {{ item.name }}
+</div>
+--------
+//item in items: items adalah array yang akan diiterasi, dan item adalah elemen individu dari array tersebut. :key: Atribut key penting untuk membantu Vue mengidentifikasi elemen secara unik, terutama saat melakukan pembaruan DOM. Biasanya, ini dihubungkan ke properti unik seperti id.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
